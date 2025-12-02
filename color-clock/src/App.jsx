cd0ef4a339719bc5a5e8099675adbc0b863629e7
Katherine <kaburbank@gmail.com>
2025-12-02 11:39:59 -0700

    successfully running clock

--- color-clock/src/App.jsx
+++ color-clock/src/App.jsx
@@ -1,38 +1,24 @@
-import { useState, useEffect } from 'react';
-import {format} from 'date-fns';
-import './App.css';
-import './index.css';
+import {format} from "date-fns";
+import "./App.css";
+import {useState, useEffect} from "react";
 
-function App() {
-  const [glowColor, setGlowColor] = useState(null);
-  const [now, setNow] = useState(new Date());
+function App(){
+  //Define a state to hold time and date
+  const [currentTime, setCurrentTime] = useState(new Date());
 
-  //Define use effect to set the clock to update every second
+  //Define use effect to update the clock every second
   useEffect(() => {
     const timer = setInterval(() => {
-      setNow(new Date());
+      setCurrentTime(new Date())
     }, 1000);
     return () => clearInterval(timer);
   }, []);
 
-  //Define constant to generate a random color
-  const randomColor = () => {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
-  };
-
-  //Define constant to set the glow color to the random color
-  const handleGlow = () => {
-    setGlowColor(randomColor());
-  };
-
   return (
-    <div className="container">
-        <button onClick={handleGlow}>Change Glow</button>
-        <p className="clock" style={glowColor ? {boxShadow: `0 0 20px ${glowColor}, 0 0 40px ${glowColor}`, transition: 'box-shadow 0.3s ease-in-out'} : {}}>
-        {format(now, 'yyyy-MM-dd h:mm:ss aaa')}
-        </p>
-    </div>
+    <p className="clock">
+    {format (currentTime, "MMMM do yyyy, h:mm:ss a")}
+    </p>
   );
 };
 
-export default App
+export default App;
